866f5ff9b2a6eb142f1a1f5853d6c28e47fe24da
Pavel Ševčík <pajaseviwow@gmail.com>
2016-02-22 10:42:15 +0100

    editable homepage components

--- page.php
+++ page.php
@@ -28,9 +28,26 @@ if (is_front_page() ) {
     $context['header_transparent'] = 'header--transparent';
 }
 
-$context['hero_elements'] = types_child_posts('hero-element');
-$heroElement = new TimberPost(75);
-$context['hero_element'] = $heroElement;
-var_dump($heroElement);
+$timber_carousel_items = array();
+$carousel_item = types_child_posts('carousel-item');
+foreach ($carousel_item as $carousel_item) {
+    array_push($timber_carousel_items, new TimberPost($carousel_item->ID));
+}
+
+$timber_hero_elements = array();
+$hero_elements = types_child_posts('hero-element');
+foreach ($hero_elements as $hero_element) {
+    array_push($timber_hero_elements, new TimberPost($hero_element->ID));
+}
+
+$timber_citation_elements = array();
+$citations = types_child_posts('citation');
+foreach ($citations as $citation) {
+    array_push($timber_citation_elements, new TimberPost($citation->ID));
+}
+
+$context['carousel_items'] = $timber_carousel_items;
+$context['hero_elements'] = $timber_hero_elements;
+$context['citations'] = $timber_citation_elements;
 
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );

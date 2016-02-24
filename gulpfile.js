6f7fcdb507d2c2dde2666edcdae1ed2eef9e69a8
Pavel Ševčík <pajaseviwow@gmail.com>
2016-02-24 14:37:51 +0100

    added stylelint, fixed linted bugs

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,12 @@ var http = require('http');
 var st = require('st');
 
 var PATHS = {
-    CSS : [
+    VENDOR_CSS : [
         './node_modules/normalize.css/normalize.css',
+        './src/css/fontello.css'
+    ],
+    CSS : [
         './src/css/app.css',
-        './src/css/fontello.css',
         './src/css/components/*.css'
     ],
     JS : [
@@ -32,15 +34,64 @@ var PATHS = {
     ]
 }
 
-gulp.task('styles', ['images'], function() {
+var stylelintConfig = {
+    'plugins': [
+      'stylelint-statement-max-nesting-depth'
+    ],
+    'rules': {
+        'block-no-empty': true,
+        'color-no-invalid-hex': true,
+        'declaration-colon-space-after': 'always',
+        'declaration-colon-space-before': 'never',
+        'function-comma-space-after': 'always',
+        'function-url-quotes': 'double',
+        'media-feature-colon-space-after': 'always',
+        'media-feature-colon-space-before': 'never',
+        'media-feature-name-no-vendor-prefix': true,
+        'max-empty-lines': 5,
+        'number-leading-zero': 'always',
+        'number-no-trailing-zeros': true,
+        'property-no-vendor-prefix': true,
+        'rule-no-duplicate-properties': true,
+        'declaration-block-no-single-line': true,
+        'rule-trailing-semicolon': 'always',
+        'selector-list-comma-space-before': 'never',
+        'selector-list-comma-newline-after': 'always',
+        'selector-no-id': true,
+        'string-quotes': 'double',
+        'value-no-vendor-prefix': true,
+        'statement-max-nesting-depth': [3, { countAtRules: false }]
+    }
+}
+
+gulp.task('stylelint', function() {
     return gulp.src(PATHS.CSS)
         .pipe(postcss([
-            require("postcss-import")(),
-            require("postcss-url")(),
-            require("postcss-cssnext")(),
-            require("cssnano")({unused: false, reduceIdents: false, mergeIdents: false, zindex: false}),
-            require("postcss-browser-reporter")(),
-            require("postcss-reporter")(),
+            require('stylelint')(stylelintConfig),
+            require('postcss-reporter')({
+                    clearMessages: true,
+                    throwError: true
+            })
+        ]));
+});
+
+gulp.task('styles', ['stylelint', 'images'], function() {
+    return gulp.src(PATHS.VENDOR_CSS.concat(PATHS.CSS))
+        .pipe(postcss([
+            require('postcss-import')(),
+            require('postcss-url')(),
+            require('postcss-cssnext')(),
+            require('cssnano')({
+                    unused: false,
+                    reduceIdents: false,
+                    mergeIdents: false,
+                    zindex: false
+            }),
+            require('postcss-browser-reporter')(),
+            require('postcss-reporter')({
+                    clearMessages: true,
+                    throwError: true
+            })
         ]))
         .pipe(base64({
             baseDir: './',

5fff7ab6c5ebeb37e7459f6798b6c8fb21a05513
Pavel Ševčík <pajaseviwow@gmail.com>
2016-03-04 16:54:24 +0100

    fbevents

--- fbevents.php
+++ fbevents.php
@@ -3,13 +3,13 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-$app_id = "xxxxxxxx";
-$app_secret = "xxxxxx";
+$app_id = "475486162662346";
+$app_secret = "c9887bb8358b2a88a5d83dc9c3bf6d2e";
 
 
 require 'vendor/autoload.php';
 
-require __DIR__ . '/fb/autoload.php';
+require __DIR__ . '/vendor/facebook/php-sdk-v4/autoload.php';
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
@@ -23,7 +23,7 @@ $session = new FacebookSession($app_id."|".$app_secret);
 
 try {
     // Get a list of pages with you as admin
-    $request = new FacebookRequest($session, 'GET', '/'.$_GET["page"].'/events');
+    $request = new FacebookRequest($session, 'GET', '/vejdiven/events');
 
     $pageList = $request->execute()->getGraphObject()->asArray();
 
